914e9d90002b647797798f8aca3d95ad2d5a5b4b
Danil Korotenko <danil.korotenko@gmail.com>
2024-11-28 12:29:13 +0200

    Initial Commit

---
+++ AirDropController/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  AirDropController
+//
+//  Created by Danil Korotenko on 11/28/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ AirDropController/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  AirDropController
+//
+//  Created by Danil Korotenko on 11/28/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
